eddee0e78b632de2b5d1506c3083ba28aca46f8d
Eric Williams <eric.williams@contino.io>
2021-04-12 22:19:26 -0600

    Adds a json logger

--- logger.go
+++ logger.go
@@ -1,12 +1,32 @@
 package logger
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"time"
 )
 
 func Info(message string) {
 	fmt.Printf("[Info] %s: %s\n", timeFormat(), message)
+}
+
+func InfoJson(message string) {
+
+	type LogEntry struct {
+		Level   string
+		Time    string
+		Message string
+	}
+
+	logEntry := LogEntry{"Info", timeFormat(), message}
+
+	prettyJSON, err := json.MarshalIndent(logEntry, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s\n", string(prettyJSON))
 }
 
 func timeFormat() string {
